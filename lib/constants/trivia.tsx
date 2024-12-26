8ec8e68939371fc3d6fbd1333ac8884602d96cc5
k14lb3 <k14lb3@gmail.com>
2024-12-26 13:26:46 +0800

    ♻️ Convert hard-coded icons to constants

--- lib/constants/trivia.tsx
+++ lib/constants/trivia.tsx
@@ -3,12 +3,12 @@ import { ICONS } from '@lib';
 
 export const TRIVIA = [
   <>
-    I use <Link href='https://archlinux.org/'>󰣇rch</Link> and{'  '}
-    <Link href='https://neovim.io/'>eovim</Link>{' '}
+    I use <Link href='https://archlinux.org/'>{ICONS.ARCH}rch</Link> and{'  '}
+    <Link href='https://neovim.io/'>{ICONS.NEOVIM}eovim</Link>{' '}
     <Link href='https://knowyourmeme.com/memes/btw-i-use-arch'>BTW</Link>.
   </>,
   <>
-    I drink a lot of {' '}
+    I drink a lot of {ICONS.COFFEE}{' '}
     <Link href='https://knowyourmeme.com/memes/coffee-spelled-backwards-is-eeffoc'>
       eeffoc
     </Link>.
